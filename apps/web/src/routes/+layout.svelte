9f51ffb3c9bbab5147c0839c42147379651105b7
Michał Muzyka <michal.muzyka@proton.me>
2026-05-04 20:54:21 +0200

    Dark mode support and some shadcn components

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -2,8 +2,12 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
+  	import { ModeWatcher } from "mode-watcher";
+
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
+
