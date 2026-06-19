e989e743c30d544cfa60a5bf198e3c1df3be1047
Michał Muzyka <michal.muzyka@proton.me>
2026-06-19 14:42:40 +0200

    Web: initialize userPrefferences using data from cookie

--- apps/web/src/routes/(app)/+layout.server.ts
+++ apps/web/src/routes/(app)/+layout.server.ts
@@ -1,13 +1,35 @@
+import { defaultUserPreferences, UserPreferencesSchema } from "@repo/schema";
 import type { LayoutServerLoad } from "./$types";
+import { safeParse } from "valibot";
 
-export const load: LayoutServerLoad = async ({ locals, request }) => {
+export const load: LayoutServerLoad = async ({ locals, request, cookies }) => {
     const res = await locals.api.v1.auth.me.$get();
 
     if (!res.ok) {
-        return { user: null, locationPath: new URL(request.url).pathname };
+        return {
+            user: null,
+            locationPath: new URL(request.url).pathname,
+            preferences: defaultUserPreferences
+        };
     }
 
     const user = await res.json();
+
+    let preferences = defaultUserPreferences;
+    const prefsCookie = cookies.get('gorka_prefs');
+
+    if (prefsCookie) {
+        try {
+            const parsedJson = JSON.parse(decodeURIComponent(prefsCookie));
+            const result = safeParse(UserPreferencesSchema, parsedJson);
+
+            if (result.success) {
+                preferences = result.output;
+            }
+        } catch (e) {
+            console.error('Error while parsing preferences cookie, using defaults.', e);
+        }
+    }
 
     return {
         user: {
@@ -16,5 +38,6 @@ export const load: LayoutServerLoad = async ({ locals, request }) => {
             name: user.name,
         },
         locationPath: new URL(request.url).pathname,
+        preferences
     };
 }
--- apps/web/src/routes/(app)/+layout.svelte
+++ apps/web/src/routes/(app)/+layout.svelte
@@ -1,25 +1,32 @@
 <script lang="ts">
-    import * as Sidebar from "$lib/components/ui/sidebar/index.js";
-    import AppSidebar from "$lib/components/app-sidebar.svelte";
-    import ThemeToggler from "$lib/components/theme-toggler.svelte";
-    import MobileSidebarTrigger from "$lib/components/mobile/mobile-sidebar-trigger.svelte";
+    import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+    import AppSidebar from '$lib/components/app-sidebar.svelte';
+    import ThemeToggler from '$lib/components/theme-toggler.svelte';
+    import MobileSidebarTrigger from '$lib/components/mobile/mobile-sidebar-trigger.svelte';
 
-    import logoImage from "$lib/assets/logo.svg"
-    import Button from "$lib/components/ui/button/button.svelte";
- 
+    import logoImage from '$lib/assets/logo.svg';
+    import Button from '$lib/components/ui/button/button.svelte';
+    import { userPreferences } from '$lib/states/preferences.svelte';
+
     let { children, data } = $props();
+
+    userPreferences.init(data.preferences);
 </script>
 
 <Sidebar.Provider class="h-dvh">
-    <AppSidebar locationPath={data.locationPath} user={data?.user}/>
-    <main class="w-full h-full">
-        <header class="px-2 md:px-4 p-2 flex items-center justify-between border-b border-border w-full">
+    <AppSidebar locationPath={data.locationPath} user={data?.user} />
+    <main class="h-full w-full">
+        <header
+            class="border-border flex w-full items-center justify-between border-b p-2 px-2 md:px-4"
+        >
             <div class="flex items-center gap-2">
                 <Sidebar.Trigger class="hidden md:block" />
-                <MobileSidebarTrigger class="md:hidden"/>
+                <MobileSidebarTrigger class="md:hidden" />
             </div>
-            <Button href="/" variant="ghost"><img src={logoImage} alt="logo" class="md:hidden"></Button>
-            <ThemeToggler/>
+            <Button href="/" variant="ghost"
+                ><img src={logoImage} alt="logo" class="md:hidden" /></Button
+            >
+            <ThemeToggler />
         </header>
         {@render children?.()}
     </main>
